d537ce6785eb2d9e219d211f31112d8617049176
Ish Tahir <ishtahir@icloud.com>
2018-12-05 16:16:46 -0600

    Li's are now removed completely when X is pressed

--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,12 @@
-// check off todos by clicking on them
+// clear todos when they are clicked
 $('li').click(function() {
     $(this).toggleClass('clear');
+});
+
+// delete todos when the 'X' is clicked
+$('.trash').click(function(event) {
+    $(this).parent().fadeOut(800, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
 });
