85e1c4ec1e23a3e039ce001a59dc2513cb30eaae
Ish Tahir <ishtahir@icloud.com>
2018-12-06 16:45:07 -0600

    Added event listener for input to add new li's when enter is pressed

--- js/todo.js
+++ js/todo.js
@@ -5,8 +5,20 @@ $('li').click(function() {
 
 // delete todos when the 'X' is clicked
 $('.trash').click(function(event) {
-    $(this).parent().fadeOut(800, function() {
+    $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+// add the keyup listener for when enter is pressed
+$('input[type="text"]').keyup(function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        const value = $(this).val();
+        let text = '';
+        text += `<li><span class="trash">X</span> ${value}</li>`;
+        $('.todo-list-items').append(text);
+        $(this).val('');
+    }
 });
